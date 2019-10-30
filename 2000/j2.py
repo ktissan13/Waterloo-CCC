778e57583d2290e215371ad4aed1caaa3684650a
Tissan Kugathas <32685188+ktissan13@users.noreply.github.com>
2019-10-30 13:03:12 -0400

    Update j2.py

--- 2000/j2.py
+++ 2000/j2.py
@@ -3,18 +3,23 @@
 # ICS4U0
 # October 3O 2019
 
-m = 1
-n = 100
+m = int(input())
+n = int(input())
 final = 0
 
 if 1 <= m <= n <= 32000:
     for num in range(m,n,1):
         string = str(num)
         temp = 0
-        for elem in string:
-            if elem == '1' or elem == '8' or elem == '6' or elem == '9' or elem == '0':
+        backwards = ''
+        for elem in reversed(range(len(string))):
+            if string[elem] == '1' or string[elem] ==  '8' or string[elem] == '0':
                 temp += 1
-        if len(string) == temp:
+            if num > 10:
+                if string[elem] == '6' or string[elem] == '9':
+                    temp += 1
+            backwards += string[elem]
+        if len(string) == temp and string == backwards:
             final += 1
 
 print(final)
