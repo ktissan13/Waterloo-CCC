281bc18eb4c40308c6c7a2c97d883ad4dff3c695
Tissan Kugathas <32685188+ktissan13@users.noreply.github.com>
2019-11-01 12:38:56 -0400

    Create j1.py

---
+++ 2004/j1.py
@@ -0,0 +1,15 @@
+# 04 j1 squares
+# Tissan Kugathas
+# ICS4U0
+# November 1 2019
+
+import math
+
+#x = int(input())
+
+final = 2.56//1
+
+#if x != final**2:
+    #final -= 1
+
+print('The largest square has side length {}.'.format(final))
