929c4f512d2e919a81d14c84e905f1d1bf630000
Tissan Kugathas <32685188+ktissan13@users.noreply.github.com>
2019-11-01 12:14:49 -0400

    Update j3.py

--- 2000/j3.py
+++ 2000/j3.py
@@ -3,28 +3,31 @@
 # ICS4U0
 # October 31 2019
 
-played = [3,10,4]
+
 win = [35,100,10]
 prize = [30,60,9]
 
-m = 48
+#m = 48
 m_played = 0
 
-for i in range(3):
-    if m>0:
-        loop = True
-        while loop:
-            m_played += 1
-            played[i] += 1
-            m -= 1
-            if played[i] == win[i]:
-                print(m)
-                print(m_played)
-                m += prize[i]
-                played[i] = 0
-                loop = False
-                print(m)
-            elif m == 0:
-                loop = False
+m = int(input())
+a = int(input())
+b = int(input())
+c = int(input())
 
-print(m_played)
+if 0 <= a < 35 and 0 <= b < 100 and 0 <= c < 10:
+    played = [a,b,c]
+
+if 1 <= m <= 1000:
+    while m > 0:
+        for i in range(3):
+            if m > 0:
+                m_played += 1
+                played[i] += 1
+                m -= 1
+                if played[i] == win[i]:
+                    m += prize[i]
+                    played[i] = 0
+
+
+print('Martha plays {} times before going broke.'.format(m_played))
