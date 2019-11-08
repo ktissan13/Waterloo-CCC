caeb1ca1611868d031260abe4518a66cbb6e4070
Tissan Kugathas <32685188+ktissan13@users.noreply.github.com>
2019-11-07 20:09:18 -0500

    Create S2.py

---
+++ 2006/S2.py
@@ -0,0 +1,44 @@
+# CCC 06 S2 Attack of the Cipher Text
+# Tissan Kugathas
+# ICS4U0
+# November 5 2019
+
+#a = input()
+#b = input()
+#c = input()
+
+space = 32
+min = 65
+max = 90
+period = 46
+
+a = "THERE ARE NOT ENOUGH LETTERS"
+b = "XQAZASEZASNYXSANYLWQSTAXXAZM"
+c = "XFABSFAWFSZACBEAQFPQMFAEPJOHAWFSZACBEAUIJOHTAIBAIB"
+
+
+final = ''
+i = 0
+for char in b:
+    constant = ord(a[i]) - ord(b[i])
+    i += 1
+    if i > len(b):
+        i = 0
+    if ord(char) == space:
+        if constant > 0:
+            new_letter = min
+        else:
+            new_letter = max
+    else:
+        new_letter = ord(char) + constant
+    if not min <= new_letter <= max:
+        if min > new_letter and (min-new_letter) != 1 and (min-new_letter) > 0:
+            new_letter = min + (min - new_letter)
+        elif max < new_letter and (new_letter-max) != 1 and (new_letter-max) > 0:
+            new_letter = max - (new_letter-max)
+        else:
+            new_letter = space
+    final += chr(new_letter)
+
+print(final)
+print(constant,ord('b'),ord(' '),ord('Q'))
