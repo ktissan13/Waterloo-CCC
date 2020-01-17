01c948390791c40445c849ff914313e559ab8ed5
Tissan Kugathas <32685188+ktissan13@users.noreply.github.com>
2020-01-16 19:18:49 -0500

    Update s3.py

---
+++ 2018/s3.py
@@ -0,0 +1,16 @@
+# CCC 18 S3 - RoboThieves
+# Tissan Kugathas
+# ICS4U0
+# January 16 2020
+
+grid = Type TabNine: : no_sem to suppress this message.[]
+
+user_input = input()
+
+user_input = user_input.split()
+
+n = user_input[0]
+m = user_input[1]
+
+if 4 <= n and m <= 100:
+    for colum in range(n):
