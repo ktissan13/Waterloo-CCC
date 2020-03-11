4e78bc95fd3b768b3950fc3ce13f764b7d28b8c5
Tissan Kugathas <32685188+ktissan13@users.noreply.github.com>
2020-03-11 19:09:14 -0400

    Update j4.py

--- 2000/j4.py
+++ 2000/j4.py
@@ -3,4 +3,17 @@
 # ICS4U0
 # November 1 2019
 
-n = 3
+stream = [[]]
+
+n = int(input())
+
+status = True
+index = 0
+
+while status:
+    user_input = int(input())
+
+    if user_input != 99 and user_input != 88:
+        stream[index].append(user_input)
+    elif num == n-1 and user_input = 77:
+        status = False
